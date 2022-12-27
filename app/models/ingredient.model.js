d12a69333dd1fc8db17566333c2949c77df409a4
AndrewSebastianH <90231686+AndrewSebastianH@users.noreply.github.com>
2022-12-27 14:05:01 +0700

    Update ingredient.model.js
    added imageurl

--- app/models/ingredient.model.js
+++ app/models/ingredient.model.js
@@ -5,7 +5,8 @@ module.exports = mongoose => {
             name: String,
             category: String,
             stock: Number,
-            price: Number
+            price: Number,
+            imageUrl: String
         },
         {
             timestamps: true
